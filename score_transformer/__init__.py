b0b21c500165912d1a25414ea5f75f3870cc58d6
Tim Beyer <tim.beyer@tum.de>
2023-07-27 20:23:59 +0200

    Simplify

---
+++ score_transformer/__init__.py
@@ -0,0 +1,4 @@
+from .metric.ScoreSimilarity import score_similarity
+# from .metric.ScoreSimilarity_orig import score_similarity as score_similarity_orig
+from .tokenization_tools.detokenizer.tokens_to_score import tokens_to_score
+from .tokenization_tools.tokenizer.score_to_tokens import MusicXML_to_tokens as score_to_tokens
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='score_transformer',
+    version='0.1',
+    description='score_transformer',
+    packages=["score_transformer"]
+)
